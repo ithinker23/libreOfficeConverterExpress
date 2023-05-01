ba540fe7282c4989483e7ae49bdbf24be70488a9
ithinker23 <44293948+ithinker23@users.noreply.github.com>
2023-04-30 23:42:31 -0700

    added 1 hour Timeout

--- controllers/convertController.js
+++ controllers/convertController.js
@@ -2,6 +2,8 @@ const converter = require('../services/converter')
 
 module.exports = {
     convertDocxToPDF : (req, res)=>{
+        req.setTimeout(1000*60*60);
+        
         let dir = req.body.dir
         
         console.log(req.body)
